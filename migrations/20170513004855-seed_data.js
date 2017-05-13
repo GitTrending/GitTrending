849a1400e120b349f9fe6ea43b2f5abbd4206f6c
Shengtao Li <sevenlist0110@gmail.com>
2017-05-12 19:29:37 -0700

    [update] add seed data

---
+++ migrations/20170513004855-seed_data.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const Promise = require('bluebird');
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('users',[
+      {
+        github_id: 'John.Doe',
+      },
+      {
+        github_id: 'Jane.Doe',
+      },
+    ])
+    .then(()=> {
+      return Promise.all([
+        queryInterface.bulkInsert('repos',[
+          {
+            repo_name: 'React',
+            repo_description: 'A declarative, efficient, and flexible JavaScript library for building user interfaces',
+            repo_link: 'https://github.com/facebook/react',
+            repo_img: 'https://www.tutorialspoint.com/reactjs/images/reactjs-mini-logo.jpg',
+            userId: 1
+          },
+        ]), 
+        queryInterface.bulkInsert('topics',[
+          {
+            topic_name: 'React',
+            userId: 1
+          },
+        ]),
+        queryInterface.bulkInsert('keywords',[
+          {
+            keyword_name: 'JavaScript',
+            userId: 1
+          },
+        ]), 
+      ])
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('users', 
+      { github_id: 
+        {
+          $in: 
+          [
+            'John.Doe',
+            'Jane.Doe',
+          ]
+        }
+      }, 
+      {}, 
+      {primaryKeys:[],primaryKeyAttributes:[]}
+    );
+  }
+};
