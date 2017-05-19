4d2c990bb73452660f68206316e29f1970c1a924
mkearney@google.com <mkearney@google.com>
2017-05-18 21:17:15 -0700

    bleh bleh

--- config/config.js
+++ config/config.js
@@ -1,31 +1,22 @@
 const config = {
-  // "development": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "gitTrending_db",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
-  // "test": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "gittrendingtest_db",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "gitTrending_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "gittrendingtest_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
   "production": {
-    "username": "rpf6cvu92wulobtp",
-    "password": "j9ly9sfl9b4je647",
-    "database": "yd9lprtxx3hjf329",
-    "host": "s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 };
 
-// if (process.ENV === 'production') {
-  module.exports = config;	
-// } else if (process.ENV === 'test') {
-//   module.exports = config.test;
-// } else if (process.ENV === 'development') {
-//   module.exports = config.development;
-// }
+module.exports = config[process.env.NODE_ENV || 'development'];
