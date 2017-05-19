58497ec5afabf4a45b917c449416a1593d4792ea
mkearney@google.com <mkearney@google.com>
2017-05-18 21:06:52 -0700

    bleh

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require('../config/config');
+var config    = require('../config/config.js');
 var db        = {};
 
 if (config.use_env_variable) {
