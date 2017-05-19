6e3ea7cb00d7baaa3336a5ab5b658e17e87cfb77
mkearney@google.com <mkearney@google.com>
2017-05-18 20:49:12 -0700

    bleh

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ const config = {
   //   "dialect": "mysql"
   // },
   "production": {
-    "username": "v72iat2r2bu59onq",
-    "password": "o7bt87mhg04udarf",
-    "database": "nwbf8796jdkrgbdq",
-    "host": "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "username": "rpf6cvu92wulobtp",
+    "password": "j9ly9sfl9b4je647",
+    "database": "yd9lprtxx3hjf329",
+    "host": "s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   }
 };
