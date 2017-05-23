b65a9c267e91457c3cbbfd19f158af01968c2058
Meggin Kearney <mkearney@google.com>
2017-05-23 12:32:50 -0700

    Update test.sql

--- db/test.sql
+++ db/test.sql
@@ -3,46 +3,39 @@ CREATE DATABASE gittrendingtest_db;
 USE gittrendingtest_db;
 
 --
--- Table structure for table `keywords_repos`
+-- Table structure for table `users`
 --
-CREATE TABLE `keywords_repos` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `repoId` int(11) DEFAULT NULL,
-  `keywordId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `repoId` (`repoId`),
-  KEY `keywordId` (`keywordId`),
-  CONSTRAINT `keywords_repos_ibfk_1` FOREIGN KEY (`repoId`) REFERENCES `repos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `keywords_repos_ibfk_2` FOREIGN KEY (`keywordId`) REFERENCES `keywords` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `github_id` varchar(255) NOT NULL,
+  `displayName` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `profileUrl` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `keywords_repos`
+-- Dumping data for table `users`
 --
+INSERT INTO `users` VALUES (1,'John.Doe',NULL,NULL,NULL,NULL),(2,'Jane.Doe',NULL,NULL,NULL,NULL),(3,'1561825','Meggin Kearney','Meggin','https://github.com/Meggin','meggin.kearney@gmail.com');
 
-INSERT INTO `keywords_repos` VALUES (1,1,1);
-
 --
--- Table structure for table `keywords_topics`
+-- Table structure for table `topics`
 --
-CREATE TABLE `keywords_topics` (
+CREATE TABLE `topics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `keywordId` int(11) DEFAULT NULL,
-  `topicId` int(11) DEFAULT NULL,
+  `topic_name` varchar(255) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `keywordId` (`keywordId`),
-  KEY `topicId` (`topicId`),
-  CONSTRAINT `keywords_topics_ibfk_1` FOREIGN KEY (`keywordId`) REFERENCES `keywords` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `keywords_topics_ibfk_2` FOREIGN KEY (`topicId`) REFERENCES `topics` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `userId` (`userId`),
+  CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `keywords_topics`
+-- Dumping data for table `topics`
 --
-
-/*!40000 ALTER TABLE `keywords_topics` DISABLE KEYS */;
-INSERT INTO `keywords_topics` VALUES (1,1,1);
+INSERT INTO `topics` VALUES (1,'React',1),(2,'JavaScript',1),(3,'progressive web apps',1),(4,'progressive web apps',1),(5,'progressive web apps',1),(6,'progressive web apps',1),(7,'progressive web apps',1),(8,'progressive web apps',1),(9,'progressive web apps',1),(10,'progressive web apps',1),(11,'progressive web apps',1),(12,'progressive web apps',1);
 
 CREATE TABLE `repos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -59,13 +52,11 @@ CREATE TABLE `repos` (
 --
 -- Dumping data for table `repos`
 --
-
 INSERT INTO `repos` VALUES (1,'React','A declarative, efficient, and flexible JavaScript library for building user interfaces','https://github.com/facebook/react',1,1),(2,'Another React','Another React','https://github.com/facebook/react',1,0),(3,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(4,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(5,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(6,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(7,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(8,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(9,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(10,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(11,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0),(12,'progressive web apps','A selection of Progressive Web Apps','https://github.com/pwarocks/pwa.rocks',1,0);
 
 --
 -- Table structure for table `repos_topics`
 --
-
 CREATE TABLE `repos_topics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `repoId` int(11) DEFAULT NULL,
@@ -76,58 +67,15 @@ CREATE TABLE `repos_topics` (
   CONSTRAINT `repos_topics_ibfk_1` FOREIGN KEY (`repoId`) REFERENCES `repos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `repos_topics_ibfk_2` FOREIGN KEY (`topicId`) REFERENCES `topics` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `repos_topics`
 --
-
 INSERT INTO `repos_topics` VALUES (1,1,1);
-/*!40000 ALTER TABLE `repos_topics` ENABLE KEYS */;
-
---
--- Table structure for table `topics`
---
-
-CREATE TABLE `topics` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `topic_name` varchar(255) NOT NULL,
-  `userId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `userId` (`userId`),
-  CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `topics`
---
-
-INSERT INTO `topics` VALUES (1,'React',1),(2,'JavaScript',1),(3,'progressive web apps',1),(4,'progressive web apps',1),(5,'progressive web apps',1),(6,'progressive web apps',1),(7,'progressive web apps',1),(8,'progressive web apps',1),(9,'progressive web apps',1),(10,'progressive web apps',1),(11,'progressive web apps',1),(12,'progressive web apps',1);
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `github_id` varchar(255) NOT NULL,
-  `displayName` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `profileUrl` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` VALUES (1,'John.Doe',NULL,NULL,NULL,NULL),(2,'Jane.Doe',NULL,NULL,NULL,NULL),(3,'1561825','Meggin Kearney','Meggin','https://github.com/Meggin','meggin.kearney@gmail.com');
 
 --
 -- Table structure for table `users_repos_favorite`
 --
-
 CREATE TABLE `users_repos_favorite` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) DEFAULT NULL,
@@ -144,5 +92,4 @@ CREATE TABLE `users_repos_favorite` (
 --
 -- Dumping data for table `users_repos_favorite`
 --
-
 INSERT INTO `users_repos_favorite` VALUES (1,1,1,1,0),(2,1,2,0,1);
