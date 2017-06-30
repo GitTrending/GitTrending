8b638a5b9b5ff41a0822bbadedfa29fd37402c65
Noel Parisi <ycleptnoel@gmail.com>
2017-06-30 12:30:34 -0700

    migration edits

--- migrations/20170515233228-repo-table-three.js
+++ migrations/20170515233228-repo-table-three.js
@@ -2,16 +2,9 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.addColumn(
-      'repos',
-      'repo_score',
-      {
-        type: Sequelize.INTEGER,
-        defaultValue: 1
-      }
-    )
+    return Promise.resolve();
   },
-
+  
   down: function (queryInterface, Sequelize) {
     return queryInterface.removeColumn(
       'repos',
